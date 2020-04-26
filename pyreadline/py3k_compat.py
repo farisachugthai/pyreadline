a1aa1c840c088202515c3bd8e203440aa5aebbba
farisachugthai <farischugthai@gmail.com>
2020-04-26 17:23:35 -0400

    Stop using collections.Callable!
    A DeprecationWarning was fired as a result of it's use
    rather than collections.abc.Callable.
    
    Since python automatically runs the `site` module
    which automatically imports readline, a DeprecationWarning
    in this library meant that every time python started,
    a warning was fired.
    
    That was giving me grays.

--- pyreadline/py3k_compat.py
+++ pyreadline/py3k_compat.py
@@ -1,28 +1,52 @@
+"""Establish python2/python3 compatibility.
+
+.. data:: PY3
+
+    Bool
+
+.. function:: callable(x)
+
+    isinstance(x, collections.abc.Callable)
+
+.. function:: execfile(fname, glob=None, loc=None)
+
+    execfile as per python2 builtins.
+
+"""
+
 from __future__ import print_function, unicode_literals, absolute_import
 import sys
 
 if sys.version_info[0] >= 3:
+    # TODO: if sys.version_info < (3,6): class ModuleNotFoundError(ImportError)
+
+    from builtins import str, bytes
+    # bytes = bytes
+    from io import StringIO
     import collections
 
     PY3 = True
 
     def callable(x):
-        return isinstance(x, collections.Callable)
+        return isinstance(x, collections.abc.Callable)
 
-    def execfile(fname, glob, loc=None):
-        loc = loc if (loc is not None) else glob
+    def execfile(fname, glob=None, loc=None):
+        glob = glob if glob is not None else globals()
+        loc = loc if loc is not None else glob
         with open(fname) as fil:
             txt = fil.read()
         exec(compile(txt, fname, "exec"), glob, loc)
 
     unicode = str
-    bytes = bytes
-    from io import StringIO
 else:
-    PY3 = False
-    callable = callable
-    execfile = execfile
-    bytes = str
-    unicode = unicode
-
+    from builtins import callable, execfile, unicode, str
     from StringIO import StringIO
+
+    PY3 = False
+    # callable = callable
+    # execfile = execfile
+    # unicode = unicode
+
+    # Wtf??
+    bytes = str
+
